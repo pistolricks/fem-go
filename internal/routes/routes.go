f3295ba5f6438e5cdace656b86815c03ff897b78
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-05-21 02:22:35 -0700

    Add middleware for user authentication handling
    Introduce `UserMiddleware` to manage user authentication via tokens. Includes `Authenticate` and `RequireUser` methods to validate tokens and enforce access control. Integrated middleware into the application with updates to the initialization logic and user store functions.

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,14 +6,17 @@ import (
 )
 
 func SetupRoutes(app *app.Application) *chi.Mux {
-
 	r := chi.NewRouter()
 
+	r.Group(func(r chi.Router) {
+		r.Use(app.Middleware.Authenticate)
+		r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+		r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+		r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
+		r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
+	})
+
 	r.Get("/health", app.Healthcheck)
-	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
-	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
-	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
-	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
 
 	r.Post("/users", app.UserHandler.HandleRegisterUser)
 	r.Post("/tokens/authentication", app.TokenHandler.HandleCreateToken)
